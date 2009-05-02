0bbb345c2ccbf4a9e09a7ee55c804d54b38ba1ca
matthieu <matthieu@cvs.openbsd.org>
2009-05-02 15:23:05 +0000

    update to libXi 1.2.1

--- lib/libXi/src/XGetDCtl.c
+++ lib/libXi/src/XGetDCtl.c
@@ -61,13 +61,11 @@ SOFTWARE.
 #include <X11/extensions/extutil.h>
 #include "XIint.h"
 
-XDeviceControl * XGetDeviceControl(dpy, dev, control)
-    register Display *
-	dpy;
-    XDevice *
-	dev;
-    int
-	control;
+XDeviceControl *
+XGetDeviceControl(dpy, dev, control)
+    register Display *dpy;
+    XDevice *dev;
+    int control;
 {
     int size = 0;
     int nbytes, i;
@@ -106,6 +104,12 @@ XDeviceControl * XGetDeviceControl(dpy, dev, control)
 	sav = d;
 	_XRead(dpy, (char *)d, nbytes);
 
+        /* In theory, we should just be able to use d->length to get the size.
+         * Turns out that a number of X servers (up to and including server
+         * 1.4) sent the wrong length value down the wire. So to not break
+         * apps that run against older servers, we have to calculate the size
+         * manually.
+         */
 	switch (d->control) {
 	case DEVICE_RESOLUTION:
 	{
@@ -116,6 +120,21 @@ XDeviceControl * XGetDeviceControl(dpy, dev, control)
 		(3 * sizeof(int) * r->num_valuators);
 	    break;
 	}
+        case DEVICE_ABS_CALIB:
+        {
+            size += sizeof(XDeviceAbsCalibState);
+            break;
+        }
+        case DEVICE_ABS_AREA:
+        {
+            size += sizeof(XDeviceAbsAreaState);
+            break;
+        }
+        case DEVICE_CORE:
+        {
+            size += sizeof(XDeviceCoreState);
+            break;
+        }
 	default:
 	    size += d->length;
 	    break;
@@ -152,6 +171,63 @@ XDeviceControl * XGetDeviceControl(dpy, dev, control)
 		*iptr++ = *iptr2++;
 	    break;
 	}
+        case DEVICE_ABS_CALIB:
+        {
+            xDeviceAbsCalibState *c = (xDeviceAbsCalibState *) d;
+            XDeviceAbsCalibState *C = (XDeviceAbsCalibState *) Device;
+
+            C->control = DEVICE_ABS_CALIB;
+            C->length = sizeof(XDeviceAbsCalibState);
+            C->min_x = c->min_x;
+            C->max_x = c->max_x;
+            C->min_y = c->min_y;
+            C->max_y = c->max_y;
+            C->flip_x = c->flip_x;
+            C->flip_y = c->flip_y;
+            C->rotation = c->rotation;
+            C->button_threshold = c->button_threshold;
+
+            break;
+        }
+        case DEVICE_ABS_AREA:
+        {
+            xDeviceAbsAreaState *a = (xDeviceAbsAreaState *) d;
+            XDeviceAbsAreaState *A = (XDeviceAbsAreaState *) Device;
+
+            A->control = DEVICE_ABS_AREA;
+            A->length = sizeof(XDeviceAbsAreaState);
+            A->offset_x = a->offset_x;
+            A->offset_y = a->offset_y;
+            A->width = a->width;
+            A->height = a->height;
+            A->screen = a->screen;
+            A->following = a->following;
+
+            break;
+        }
+        case DEVICE_CORE:
+        {
+            xDeviceCoreState *c = (xDeviceCoreState *) d;
+            XDeviceCoreState *C = (XDeviceCoreState *) Device;
+
+            C->control = DEVICE_CORE;
+            C->length = sizeof(XDeviceCoreState);
+            C->status = c->status;
+            C->iscore = c->iscore;
+
+            break;
+        }
+        case DEVICE_ENABLE:
+        {
+            xDeviceEnableState *e = (xDeviceEnableState *) d;
+            XDeviceEnableState *E = (XDeviceEnableState *) Device;
+
+            E->control = DEVICE_ENABLE;
+            E->length = sizeof(E);
+            E->enable = e->enable;
+
+            break;
+        }
 	default:
 	    break;
 	}
