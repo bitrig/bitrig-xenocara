17803241cb13ed30d5edbf4e35653fd3f2c27158
Owain G. Ainsworth <oga@nicotinebsd.org>
2012-06-15 19:05:25 +0100

    Build decode.c and ops.c from x86emu in separate C files.
    To work around clang. A day and a half of messing around trying to work
    out why ironlake intel graphics vt switching broke resulted in this
    workaround for now.
    
    Ironlake uses an int10 hack using x86emu to restore the hw state on vt
    switch (because training the fdis is fiddly and error prone when you
    don't 100% know which state you're heading for due to this being bios
    stuff). with clang this doesn't work for some reason
    
    If we build x86emu with gcc it works.
    If we build x86emu with -O0 it works.
    
    Now x86emu.c in int10 is the following, including all the bits of the
    emulator:
    
    If we build decode.c and ops.c in separate C files (with optimisation
    turned on) then it works fine. This point to the fact that this is an
    optimisation pass (probably auto inlining + some other pass) removing
    something that had side effects. Until we work out what, this lets it work.
    
    thanks to marco and drahn for suggestions.

---
+++ xserver/hw/xfree86/int10/x86emu-noopt.c
@@ -0,0 +1,12 @@
+#include <sys/types.h>
+#ifdef HAVE_XORG_CONFIG_H
+#include <xorg-config.h>
+#endif
+
+//#include "debug.c"
+//#include "decode.c"
+//#include "fpu.c"
+#include "ops.c"
+//#include "ops2.c"
+//#include "prim_ops.c"
+//#include "sys.c"
--- xserver/hw/xfree86/int10/x86emu.c
+++ xserver/hw/xfree86/int10/x86emu.c
@@ -6,7 +6,7 @@
 #include "debug.c"
 #include "decode.c"
 #include "fpu.c"
-#include "ops.c"
+//#include "ops.c"
 #include "ops2.c"
 #include "prim_ops.c"
 #include "sys.c"
