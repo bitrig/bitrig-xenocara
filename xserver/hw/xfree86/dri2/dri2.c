671a385c21f59305422aab6d923c9b88fbc056b3
Owain G. Ainsworth <oga@nicotinebsd.org>
2012-06-18 15:21:45 +0100

    Initialise some variables before we do xorg_list operations on them.
    This should not strictly be necessary, since the operation that is causing
    unitialised variable warnings is:
    
        (void *)((char *)(ptr)                                              \
    		 - ((char *)&(sample)->member - (char *)(sample)))
    
    Which doesn't actually depend on the value of ``sample'' at all.
    However, it seems that llvm's optimised detects this as undefined
    behaviour and just makes it crash as soon as it is used (glxinfo crashes
    the xserver), so for now initialise them until a solution can be found.
    
    There are a few other instances of this in xf86Xinput.c, xfixes/cursor.c
    and dix/event.c but they don't seem to align the stars quite right.

--- xserver/hw/xfree86/dri2/dri2.c
+++ xserver/hw/xfree86/dri2/dri2.c
@@ -239,7 +239,7 @@ typedef struct DRI2DrawableRefRec {
 static DRI2DrawableRefPtr
 DRI2LookupDrawableRef(DRI2DrawablePtr pPriv, XID id)
 {
-    DRI2DrawableRefPtr ref;
+    DRI2DrawableRefPtr ref = NULL;
 
     xorg_list_for_each_entry(ref, &pPriv->reference_list, link) {
         if (ref->id == id)
@@ -306,7 +306,7 @@ DRI2DrawableGone(pointer p, XID id)
 {
     DRI2DrawablePtr pPriv = p;
     DRI2ScreenPtr ds = pPriv->dri2_screen;
-    DRI2DrawableRefPtr ref, next;
+    DRI2DrawableRefPtr ref = NULL, next;
     WindowPtr pWin;
     PixmapPtr pPixmap;
     DrawablePtr pDraw;
@@ -587,7 +587,7 @@ static void
 DRI2InvalidateDrawable(DrawablePtr pDraw)
 {
     DRI2DrawablePtr pPriv = DRI2GetDrawable(pDraw);
-    DRI2DrawableRefPtr ref;
+    DRI2DrawableRefPtr ref = NULL;
 
     if (!pPriv || !pPriv->needInvalidate)
         return;
