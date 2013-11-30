c2b158faa395ba1d862845eff291e4004ad53051
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2013-11-30 17:04:52 +0100

    Fix radeon kms with clang.
    My machine would segfault X as soon with my Radeon HD6770, I figured
    out that disabling optimizations fixed, as well as building it with
    gcc, further research showed others have the same issue:
    
    https://bugs.freedesktop.org/show_bug.cgi?id=47160
    
    Initializing the pointer to NULL on radeon_kms.c is enough for clang.
    I went over the warnings and fixed it.

--- driver/xf86-video-ati/src/radeon_kms.c
+++ driver/xf86-video-ati/src/radeon_kms.c
@@ -270,7 +270,7 @@ static void
 radeon_dirty_update(ScreenPtr screen)
 {
 	RegionPtr region;
-	PixmapDirtyUpdatePtr ent;
+	PixmapDirtyUpdatePtr ent = NULL;
 
 	if (xorg_list_is_empty(&screen->pixmap_dirty_list))
 		return;
